411d577bffd4dc2e8c5fd6b3f4dcfff91c273e39
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-11-20 13:17:30 +0000

    Adir Asset Test
    Began coupling Unity Nav Mesh Agent with Adir animated asset.
    
    CURRENTLY NOT WORKING - Need to create variable to supply nav mesh agent movement to create effective boolean for animator controller - considering switch to float value.

--- SteppingStones/Assets/PlayerController.cs
+++ SteppingStones/Assets/PlayerController.cs
@@ -5,24 +5,69 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
 
-    NavMeshAgent agent;
+    public Camera IsoCam;
+    public NavMeshAgent Agent;
+    Animator AdirAnim;
+    public float AdirVelocity;
+    private Rigidbody Rb;
+    //private bool Walking= false;
+
+
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        AdirAnim = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
+
+
         if (Input.GetMouseButtonDown(0))
         {
+            Ray ray = IsoCam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            float velocity = Agent.velocity.magnitude;
 
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            if (Physics.Raycast(ray, out hit))
             {
-                agent.destination = hit.point;
+                //Move the Player
+
+                Agent.SetDestination(hit.point);
+
+                
+                if (velocity != 0)
+                {
+                    Debug.Log("Moving");
+                    AdirAnim.SetBool("Walking", true);
+                }
+                else
+                {
+                    AdirAnim.SetBool("Walking", false);
+                }
+
+
+                //Walking = true;
+                //Animate();
             }
         }
+
     }
+
+    //void Animate()
+    //{
+    //    if (Walking == true )
+    //    {
+    //        Debug.Log("Moving");
+    //        AdirAnim.SetBool("Walking", true);
+    //    }
+
+    //    if (Walking == false)
+    //    {
+    //        AdirAnim.SetBool("Walking", false);
+    //    }
+
+    //}
 }
 
