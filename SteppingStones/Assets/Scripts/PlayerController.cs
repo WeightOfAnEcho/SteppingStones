74c94ab0e911beb08ad86e7cd28e6e31f0f15d3c
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-11-27 18:11:20 +0000

    Prototype Ver 06
    SOLVED - sorted issue where nav mesh ray cast would not link to correct location - Asset Forge imports do not generate colliders!

--- SteppingStones/Assets/Scripts/PlayerController.cs
+++ SteppingStones/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
     public Camera IsoCam; //Main camera reference for RayCasting
     public NavMeshAgent Agent; //Reference for the Agent (player)
     Animator AdirAnim; // Reference for the Player animator attached
@@ -19,13 +19,12 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0)) // When the left mouse button is pressed
         {
             Ray ray = IsoCam.ScreenPointToRay(Input.mousePosition); // Fire a ray from the main camera to the click position
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit)) // if the hit coordiantes are on a valid location on the Nav Mesh
+            if (Physics.Raycast(ray, out hit, 500)) // if the hit coordiantes are on a valid location on the Nav Mesh
             {
                 //if the raycast hits the Staff game object
                 if (hit.collider.gameObject.tag == "Staff")
@@ -42,15 +41,16 @@ public class PlayerController : MonoBehaviour {
                         hit.transform.SendMessage("HitByRay");
                         print("HIT Rock");
                     }
-
                 }
 
                 else if (hit.collider.gameObject.tag != "Staff")
                 {
                     //Move the Player and start the walking animation
+                    print(hit.collider.gameObject.name);
                     Agent.SetDestination(hit.point);
                 }
 
+                //Debug.DrawRay(ray.origin, (ray.direction - IsoCam.transform.position) * 10, Color.red, 5);
             }
         }
 
@@ -58,14 +58,11 @@ public class PlayerController : MonoBehaviour {
         {
             AdirAnim.SetBool("Walking", true);
         }
+
         else
         {
             AdirAnim.SetBool("Walking", false);
         }
-
     }
-
-
-
 }
 
