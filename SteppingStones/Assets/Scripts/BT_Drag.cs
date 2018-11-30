d49a9dc121a275f830395e4ca005c39967c87a79
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-11-30 17:29:50 +0000

    Prototype Ver 10
    ADDED: Sound and visual feedback on click location - normal reactive.
    
    ADDED: amdient game music - current deativated in heriarchy for sanity purposes.
    
    Tidied up assets - renamed scripts, deleted excess assets.
    
    Tweaked dragging script, now only works along a specfic plane. just requires either strong collision detection or a new solution.

---
+++ SteppingStones/Assets/Scripts/BT_Drag.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class BT_Drag : MonoBehaviour
+{
+    public Camera IsoCam;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        float distance_to_screen = IsoCam.WorldToScreenPoint(gameObject.transform.position).z;
+        Vector3 pos_move = IsoCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+        transform.position = new Vector3(pos_move.x, transform.position.y, pos_move.z);
+
+    }
+
+
+}
+
