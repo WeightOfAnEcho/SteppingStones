24977e33744163f0eff1fa3b8981af99a5383f32
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-12-01 19:49:25 +0000

    Prototype Ver 12
    ADDED: So many issues with Nav Mesh
    
    FIXED: some of them
    
    Issue with raycast restriction script - currently ignores certain height objects and needs revisiting to ensure path can be input.
    
    Need to ignore raycasts if they do not culminate in a valid, unobstructed path.

--- SteppingStones/Assets/Scripts/BT_Drag.cs
+++ SteppingStones/Assets/Scripts/BT_Drag.cs
@@ -28,9 +28,16 @@ public class BT_Drag : MonoBehaviour
 
     public void OnMouseDrag()
     {
-        float distance_to_screen = IsoCam.WorldToScreenPoint(gameObject.transform.position).z;
-        Vector3 pos_move = IsoCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
-        transform.position = new Vector3(pos_move.x, transform.position.y, pos_move.z);
+
+        Vector3 point = IsoCam.ScreenToWorldPoint(Input.mousePosition);
+        point.x = Mathf.Clamp(point.x, -44f, 27f);
+        point.y = transform.position.y;
+        point.z = transform.position.z;
+        transform.position = point;
+
+        //float distance_to_screen = IsoCam.WorldToScreenPoint(gameObject.transform.position).z;
+        //Vector3 pos_move = IsoCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+        //transform.position = new Vector3(pos_move.x, transform.position.y, pos_move.z);
 
     }
 
--- SteppingStones/Assets/Scripts/BT_Player_Controller.cs
+++ SteppingStones/Assets/Scripts/BT_Player_Controller.cs
@@ -10,7 +10,12 @@ public class BT_Player_Controller : MonoBehaviour
     Animator AdirAnim; // Reference for the Player animator attached
     public GameObject Staff; // reference for staff gameobject in scene (for picking up)
     public GameObject AttachedStaff; //reference for attached staff (when picked up)
-    public GameObject Stone;
+    public GameObject Stone1;
+    public GameObject Stone2;
+    public GameObject Stone3;
+    public GameObject Stone4;
+    public GameObject Gate1;
+    public GameObject Gate2;
 
     public GameObject ClickEffect; // reference for the effect when the ray cast lands a succesful hit on NavMesh
 
@@ -36,19 +41,44 @@ public class BT_Player_Controller : MonoBehaviour
                     print("HIT Staff");
                     AttachedStaff.SetActive(true); // set the attached staff to active
                     Staff.SetActive(false); // set the staff in the scene to false (fake picking up)
-                    Stone.GetComponent<BT_Drag>().MakeClickable();
+                    Gate1.SetActive(false);
+                    Gate2.SetActive(true);
+                    //Stone.GetComponent<BT_Drag>().MakeClickable();
                 }
 
                 NavMeshHit navmeshHit;
                 int walkableMask = 1 << NavMesh.GetAreaFromName("Walkable");
 
-                if (NavMesh.SamplePosition(hit.point, out navmeshHit, 1.0f, walkableMask))
+                if (NavMesh.SamplePosition(hit.point, out navmeshHit, 5.0f, walkableMask))
                     
                 {
                     //Move the Player and start the walking animation
                     print(hit.collider.gameObject.name);
                     Instantiate(ClickEffect, hit.point, Quaternion.LookRotation(hit.normal));
                     Agent.SetDestination(navmeshHit.position);
+                }
+
+                if (hit.collider.gameObject.tag == "Stone1") //if the raycast hits the Staff game object
+                {
+
+                    Stone1.SetActive(false);
+                    Stone2.SetActive(true);
+
+                }
+
+                if (hit.collider.gameObject.tag == "Stone2") //if the raycast hits the Staff game object
+                {
+
+                    Stone2.SetActive(false);
+                    Stone3.SetActive(true);
+
+                }
+                if (hit.collider.gameObject.tag == "Stone3") //if the raycast hits the Staff game object
+                {
+
+                    Stone3.SetActive(false);
+                    Stone4.SetActive(true);
+
                 }
 
             }
