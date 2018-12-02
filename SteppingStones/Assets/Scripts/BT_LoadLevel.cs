661de2540fcd66d1fd49a364d8a343a342908711
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-12-02 13:14:19 +0000

    Prototype Ver 13
    FIXED: Nav Mesh Navigation - player now disreagrds impossible or partial paths, will only animate and move if a full path can be walked.
    
    ADDED: a splash menu with functioning button to change scene
    
    ADDED: simple fade in and out function for the splash menu, complete with audio feedback for button press

---
+++ SteppingStones/Assets/Scripts/BT_LoadLevel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BT_LoadLevel : MonoBehaviour {
+
+    public Animator LevelLoader;
+    private int levelToLoad;
+    private AudioSource source;
+    public AudioClip Click;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void SceneTransition()
+    {
+        PlaySound();
+        LoadScene(1);
+    }
+
+    public void PlaySound()
+    {
+        source.PlayOneShot(Click);
+    }
+
+    public void LoadScene(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        LevelLoader.SetTrigger("FadeOut");
+
+        StartCoroutine("OnFadeComplete");
+    }
+
+    IEnumerator OnFadeComplete()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
--- SteppingStones/Assets/Scripts/BT_Player_Controller.cs
+++ SteppingStones/Assets/Scripts/BT_Player_Controller.cs
@@ -5,9 +5,16 @@ using UnityEngine.AI;
 
 public class BT_Player_Controller : MonoBehaviour
 {
+
+    //RAYCASTING/ CLICK TO MOVE REFERENCES ----------------------------------------------------------------------------------------
     public Camera IsoCam; //Main camera reference for RayCasting
-    public NavMeshAgent Agent; //Reference for the Agent (player)
+    private NavMeshAgent Agent; //Reference for the Agent (player)
+
+    //PLAYER ANIMATION ------------------------------------------------------------------------------------------------------------
     Animator AdirAnim; // Reference for the Player animator attached
+
+
+    //SPAWNING AND DE-SPAWNING GAME OBJECTS ----------------------------------------------------------------------------------------
     public GameObject Staff; // reference for staff gameobject in scene (for picking up)
     public GameObject AttachedStaff; //reference for attached staff (when picked up)
     public GameObject Stone1;
@@ -16,13 +23,13 @@ public class BT_Player_Controller : MonoBehaviour
     public GameObject Stone4;
     public GameObject Gate1;
     public GameObject Gate2;
-
     public GameObject ClickEffect; // reference for the effect when the ray cast lands a succesful hit on NavMesh
 
     void Start()
     {
         
         AdirAnim = GetComponent<Animator>(); // Fetch Animator component attached
+        Agent = GetComponent<NavMeshAgent>(); // Fetch the Agent Properties
     }
 
     // Update is called once per frame
@@ -33,32 +40,48 @@ public class BT_Player_Controller : MonoBehaviour
             Ray ray = IsoCam.ScreenPointToRay(Input.mousePosition); // Fire a ray from the main camera to the click position
             RaycastHit hit; //store the resulting hit
 
-            if (Physics.Raycast(ray, out hit, 500)) // if the hit coordiantes are on a valid collider
+            if (Physics.Raycast(ray, out hit, 500)) // if the hit coordiantes are on a valid collider within 500 units
             {
 
-                if (hit.collider.gameObject.tag == "Staff") //if the raycast hits the Staff game object
+                if (hit.collider.gameObject.tag == "Staff") //if the raycast hits the Staff tagged game object
                 {
                     print("HIT Staff");
+
                     AttachedStaff.SetActive(true); // set the attached staff to active
                     Staff.SetActive(false); // set the staff in the scene to false (fake picking up)
-                    Gate1.SetActive(false);
-                    Gate2.SetActive(true);
+                    Gate1.SetActive(false); //set the drawn gate to false
+                    Gate2.SetActive(true); // set the lowered gate to true
                     //Stone.GetComponent<BT_Drag>().MakeClickable();
                 }
 
-                NavMeshHit navmeshHit;
-                int walkableMask = 1 << NavMesh.GetAreaFromName("Walkable");
+                NavMeshHit navmeshHit; // provide reference for a raycast hit on the navmesh
+                int walkableMask = 1 << NavMesh.GetAreaFromName("Walkable"); // check if the nav mesh hit location was on a walkable layer/area
 
-                if (NavMesh.SamplePosition(hit.point, out navmeshHit, 5.0f, walkableMask))
+                if (NavMesh.SamplePosition(hit.point, out navmeshHit, 5, walkableMask)) //if the ray position returns a walkable sample position
                     
                 {
-                    //Move the Player and start the walking animation
                     print(hit.collider.gameObject.name);
-                    Instantiate(ClickEffect, hit.point, Quaternion.LookRotation(hit.normal));
-                    Agent.SetDestination(navmeshHit.position);
+
+
+                    NavMeshPath path = new NavMeshPath(); // create a reference for a nav mesh path
+                    Agent.CalculatePath(navmeshHit.position, path); //The agent calculates a path to the hit location on nav mesh
+                    Instantiate(ClickEffect, hit.point, Quaternion.LookRotation(hit.normal));//spawn the click effect at the hit location
+
+                    if (path.status == NavMeshPathStatus.PathPartial) // if the agent calculates an impartial path
+                    {
+                        print("no path");
+                    }
+
+                    else
+                    {
+                        print("has path");
+                        Agent.SetDestination(navmeshHit.position); // set the hit location on the nav mesh to the target destination for the agent
+                    }
+
+                    
                 }
 
-                if (hit.collider.gameObject.tag == "Stone1") //if the raycast hits the Staff game object
+                if (hit.collider.gameObject.tag == "Stone1") //if the raycast hits the Stone 1 game object
                 {
 
                     Stone1.SetActive(false);
@@ -66,14 +89,14 @@ public class BT_Player_Controller : MonoBehaviour
 
                 }
 
-                if (hit.collider.gameObject.tag == "Stone2") //if the raycast hits the Staff game object
+                if (hit.collider.gameObject.tag == "Stone2") //if the raycast hits the Stone 2 game object
                 {
 
                     Stone2.SetActive(false);
                     Stone3.SetActive(true);
 
                 }
-                if (hit.collider.gameObject.tag == "Stone3") //if the raycast hits the Staff game object
+                if (hit.collider.gameObject.tag == "Stone3") //if the raycast hits the Stone 4 game object
                 {
 
                     Stone3.SetActive(false);
@@ -84,7 +107,7 @@ public class BT_Player_Controller : MonoBehaviour
             }
         }
 
-        // ANIMATION CONTROLLING BASED ON DISTANCE FROM TARGET
+        // ANIMATION CONTROLLING BASED ON DISTANCE FROM TARGET DESTINATION -----------------------------------------------------------------------------
 
         if (Agent.remainingDistance > Agent.stoppingDistance) // If remaining distance on path is greater than stopping distance
         {
