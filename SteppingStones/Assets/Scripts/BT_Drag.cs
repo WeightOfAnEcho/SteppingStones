a237515d1a59ddfc25e55c67943d20475b5a5139
Ben Tarrant <ben.g.tarrant@gmail.com>
2018-12-06 14:04:15 +0000

    Prototype Ver 16.5
    Added new rock
    
    Tweaked drag script - still not working as intended

--- SteppingStones/Assets/Scripts/BT_Drag.cs
+++ SteppingStones/Assets/Scripts/BT_Drag.cs
@@ -7,8 +7,11 @@ using UnityEngine.UI;
 public class BT_Drag : MonoBehaviour
 {
     public Camera IsoCam;
+    Vector3 mousepos;
+    Vector3 clickedPos;
+    Vector3 offSet;
 
-    
+
 
     void Start()
     {
@@ -27,20 +30,21 @@ public class BT_Drag : MonoBehaviour
         print("new layer");
     }
 
-    //public void OnMouseDrag()
-    //{
 
-    //    Vector3 point = IsoCam.ScreenToWorldPoint(Input.mousePosition);
-    //    point.x = Mathf.Clamp(point.x, -44f, 27f);
-    //    point.y = transform.position.y;
-    //    point.z = transform.position.z;
-    //    transform.position = point;
+    public void OnMouseDrag()
+    {
 
-    //    //float distance_to_screen = IsoCam.WorldToScreenPoint(gameObject.transform.position).z;
-    //    //Vector3 pos_move = IsoCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
-    //    //transform.position = new Vector3(pos_move.x, transform.position.y, pos_move.z);
+        Vector3 point = IsoCam.ScreenToWorldPoint(Input.mousePosition);
+        point.x = Mathf.Clamp(point.x,);
+        point.y = transform.position.y;
+        point.z = transform.position.z;
+        transform.position = point;
 
-    //}
+        float distance_to_screen = IsoCam.WorldToScreenPoint(gameObject.transform.position).z;
+        Vector3 pos_move = IsoCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+        transform.position = new Vector3(pos_move.x, transform.position.y, pos_move.z);
+
+    }
 
 
 }
