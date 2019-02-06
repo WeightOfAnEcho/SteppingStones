4a1233e0cca9b75ee585a1e50ec8f72ee1941254
Ben Tarrant <ben.g.tarrant@gmail.com>
2019-02-06 13:36:25 +0000

    Level 03 Draft and Rotator
    ADDED: Draft of level 03 layout concept based on sketches
    
    ADDED: simple rotator script for valve system puzzles

---
+++ SteppingStones/Assets/Scripts/BT_Rotator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BT_Rotator : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Rotate(Vector3.up, 90, 1.0f));
+        }
+    }
+
+    IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
+    {
+        Quaternion from = transform.rotation;
+        Quaternion to = transform.rotation;
+        to *= Quaternion.Euler(axis * angle);
+
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = to;
+    }
+}
