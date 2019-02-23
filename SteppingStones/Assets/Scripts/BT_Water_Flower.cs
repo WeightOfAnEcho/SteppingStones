8a310c757634682c9ee4155293ccf8aff9ed1028
Leonardo <leonardov@protonmail.com>
2019-02-23 16:53:04 +0000

    Mechanics Showcase Scenes
    FINISHED Dragging Objects showcase scene
    
    ADDED Dragging Rock to change flow of water scene. Still needs work
    
    STARTED Knocking Down Tree scene

--- SteppingStones/Assets/Scripts/BT_Water_Flower.cs
+++ SteppingStones/Assets/Scripts/BT_Water_Flower.cs
@@ -9,6 +9,9 @@ public class BT_Water_Flower : MonoBehaviour
     private bool spawned;
     public float spawnSpeed = 0.1f;
 
+    private bool gravity = false; //not working. turns on spawning of water downwards
+    private int waterLimit = 50; //not working. limits the number of water to be spawned, as to not melt computers 
+
     public GameObject iceBlock;
 
     // Start is called before the first frame update
@@ -28,94 +31,132 @@ public class BT_Water_Flower : MonoBehaviour
         Vector3 rgt = transform.TransformDirection(Vector3.right);
         Vector3 bck = transform.TransformDirection(Vector3.back);
 
+        Vector3 dwn = transform.TransformDirection(Vector3.down);
+
+        int counter = 0; //counts how many water block have been spawned
 
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, fwd, out hit, 1))
+        if(counter<waterLimit)
         {
+            Debug.Log(counter);
 
-            if (hit.transform.tag == "Totem")
+            if (Physics.Raycast(transform.position, fwd, out hit, 1))
             {
-                hit.transform.SendMessageUpwards("HitByWater");
+
+                if (hit.transform.tag == "Totem")
+                {
+                    hit.transform.SendMessageUpwards("HitByWater");
+                }
+
+
+                if (hit.transform.tag == "Ice")
+                {
+                    Freeze();
+                }
+
             }
 
-
-            if (hit.transform.tag == "Ice")
+            else
             {
-                Freeze();
+                Vector3 end = transform.position + fwd;
+                Instantiate(waterBlock, end, transform.rotation);
+                counter++; 
+            }
+
+            if (Physics.Raycast(transform.position, rgt, out hit, 1))
+            {
+
+                if (hit.transform.tag == "Totem")
+                {
+                    hit.transform.SendMessageUpwards("HitByWater");
+                }
+
+
+                if (hit.transform.tag == "Ice")
+                {
+                    Freeze();
+                }
+            }
+
+            else
+            {
+                Vector3 end = transform.position + rgt;
+                Instantiate(waterBlock, end, transform.rotation);
+                counter++;
+            }
+
+            if (Physics.Raycast(transform.position, lft, out hit, 1))
+            {
+
+                if (hit.transform.tag == "Totem")
+                {
+                    hit.transform.SendMessageUpwards("HitByWater");
+                }
+
+
+                if (hit.transform.tag == "Ice")
+                {
+                    Freeze();
+                }
+            }
+
+            else
+            {
+                Vector3 end = transform.position + lft;
+                Instantiate(waterBlock, end, transform.rotation);
+                counter++;
+            }
+
+            if (Physics.Raycast(transform.position, bck, out hit, 1))
+            {
+
+                if (hit.transform.tag == "Totem")
+                {
+                    hit.transform.SendMessageUpwards("HitByWater");
+                }
+
+                if (hit.transform.tag == "Ice")
+                {
+                    Freeze();
+                }
+            }
+
+            else
+            {
+                Vector3 end = transform.position + bck;
+                Instantiate(waterBlock, end, transform.rotation);
+                counter++;
+            }
+
+            if (gravity)
+            {
+                if (Physics.Raycast(transform.position, dwn, out hit, 1))
+                {
+
+                    if (hit.transform.tag == "Totem")
+                    {
+                        hit.transform.SendMessageUpwards("HitByWater");
+                    }
+
+                    if (hit.transform.tag == "Ice")
+                    {
+                        Freeze();
+                    }
+                }
+
+                else
+                {
+                    Vector3 end = transform.position + bck;
+                    Instantiate(waterBlock, end, transform.rotation);
+                    counter++;
+                }
             }
 
         }
-
-        else
-        {
-            Vector3 end = transform.position + fwd;
-            Instantiate(waterBlock, end, transform.rotation);
-        }
-
-        if (Physics.Raycast(transform.position, rgt, out hit, 1))
-        {
-
-            if (hit.transform.tag == "Totem")
-            {
-                hit.transform.SendMessageUpwards("HitByWater");
-            }
-
-
-            if (hit.transform.tag == "Ice")
-            {
-                Freeze();
-            }
-        }
-
-        else
-        {
-            Vector3 end = transform.position + rgt;
-            Instantiate(waterBlock, end, transform.rotation);
-        }
-
-        if (Physics.Raycast(transform.position, lft, out hit, 1))
-        {
-
-            if (hit.transform.tag == "Totem")
-            {
-                hit.transform.SendMessageUpwards("HitByWater");
-            }
-
-
-            if (hit.transform.tag == "Ice")
-            {
-                Freeze();
-            }
-        }
-
-        else
-        {
-            Vector3 end = transform.position + lft;
-            Instantiate(waterBlock, end, transform.rotation);
-        }
-
-        if (Physics.Raycast(transform.position, bck, out hit, 1))
-        {
-
-            if (hit.transform.tag == "Totem")
-            {
-                hit.transform.SendMessageUpwards("HitByWater");
-            }
-
-            if (hit.transform.tag == "Ice")
-            {
-                Freeze();
-            }
-        }
-
-        else
-        {
-            Vector3 end = transform.position + bck;
-            Instantiate(waterBlock, end, transform.rotation);
-        }
-
     }
+
+        
 
     public void Regress() // function to be called by button
     {
