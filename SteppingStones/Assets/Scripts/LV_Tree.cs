bd541f9f1dec0c2d61ad84ae6b0e3c2d1cd8cb9c
Leonardo <leonardov@protonmail.com>
2019-02-23 19:36:07 +0000

    Knocking Tree Mechanic Scene
    working as intended. just needs a bit of polish

---
+++ SteppingStones/Assets/Scripts/LV_Tree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LV_Tree : MonoBehaviour
+{
+    public GameObject NavMeshBlock;
+    Animator tree_anim;
+    int hitCount = 0;
+    public int hitsUntilKnock = 3;
+
+    private void Awake()
+    {
+        tree_anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HIT");
+
+        if (hitCount < hitsUntilKnock)
+        {
+            hitCount++;
+        }
+        else
+        {
+            KnockTree();
+        }
+    }
+
+
+    void KnockTree()
+    {
+        tree_anim.SetTrigger("knockTree");
+        NavMeshBlock.SetActive(false);
+    }
+    
+}
