fe1ff0f4b36397e9f28943a064ad1705d93d2378
Leonardo <leonardov@protonmail.com>
2019-03-25 16:01:40 +0000

    fixed Rock Dragging

--- SteppingStones/Assets/Scripts/LV_MouseDragObject.cs
+++ SteppingStones/Assets/Scripts/LV_MouseDragObject.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pixelplacement;
+
 
 /*
  * Drags gameObject along surface (Y axis)
@@ -9,6 +11,7 @@ using UnityEngine;
 public class LV_MouseDragObject : MonoBehaviour
 {
     float planeY; //defining where ground is
+    public GameObject GO_spline;
 
     private void OnMouseDrag()
     {
@@ -17,19 +20,60 @@ public class LV_MouseDragObject : MonoBehaviour
 
     void mouseDragMoveObject()
     {
+        
         Transform draggingObject = transform;
+        Vector3 initial_position;
+        Vector3 final_position;
 
         planeY = transform.position.y;
         Plane plane = new Plane(Vector3.up, Vector3.up * planeY); // ground plane
-       
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         float distance; // the distance from the ray origin to the ray intersection of the plane
 
         if (plane.Raycast(ray, out distance))
         {
-            draggingObject.position = ray.GetPoint(distance); // distance along the ray
-        }
+ 
+            initial_position = ray.GetPoint(distance);
 
+            final_position = restrictPosToSpline(draggingObject.position);
+
+            if (!isSkipping(initial_position, final_position))
+            {
+                Vector3 mouse_pos = ray.GetPoint(distance);
+                draggingObject.position = restrictPosToSpline(mouse_pos);
+            }
+            else if(isSkipping(initial_position, final_position))
+            {
+                draggingObject.position = final_position;
+            }
+        }
+    }
+
+    private Vector3 restrictPosToSpline(Vector3 pos)
+    {
+        float splinePoint;
+        Vector3 splinePosition;
+        splinePoint = GO_spline.GetComponent<Spline>().ClosestPoint(pos, 100);
+        splinePosition = GO_spline.GetComponent<Spline>().GetPosition(splinePoint, true, 100);
+
+        return splinePosition;
+    }
+
+    private bool isSkipping(Vector3 initial_pos, Vector3 final_pos)
+    {
+        float max_distance = 1.5f;
+        float distance = Vector3.Distance(initial_pos, final_pos);
+
+        if(distance > max_distance)
+        {
+            //Debug.Log("too far away");
+            return true;
+        }
+        else
+        {
+            //Debug.Log("close enough");
+            return false;
+        }
     }
 
 }
